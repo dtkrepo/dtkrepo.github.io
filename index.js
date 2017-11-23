9f01fa410527097153ca646222a7cd2a1d052caa
lobsterzoidberg <Dmitriy_tkachenko@hotmail.com>
2017-11-22 20:23:13 -0800

    construct function
    "under construction" page is now built by a function call. Soon multiple page views will be possible.

--- index.js
+++ index.js
@@ -2,10 +2,35 @@
 // main javascript file
 "use strict";
 
-//initializes various elements of the page
-//nothing built yet
-//window.onload = function() {}
+window.onload = function() {
+    construction_page();
+}
 
+//builds the construction page
+function construction_page(){
+    var body = document.body;
+    //build heading
+    var heading = document.createElement("h1")
+    heading.setAttribute("id", "construction");
+    var txt = document.createTextNode("Under Construction");
+    heading.appendChild(txt);
+    body.appendChild(heading);
+    //build line and break
+    var line = document.createElement("hr");
+    body.appendChild(line);
+    var br = document.createElement("br");
+    body.appendChild(br);
+    //add invert button
+    var i_button = document.createElement("button");
+    txt = document.createTextNode("Invert");
+    i_button.appendChild(txt);
+    i_button.setAttribute("id","color_button");
+    i_button.setAttribute("type","button");
+    i_button.setAttribute("onclick","invert()");
+    body.appendChild(i_button);
+}
+
+//switches the color of the heading and background
 function invert(){
     var temp = getComputedStyle(document.body, null).getPropertyValue("background-color");
     if (temp != "rgb(0, 0, 0)"){
@@ -14,7 +39,6 @@ function invert(){
     }
     else{
         document.body.style.backgroundColor = "rgb(255, 127, 0)";
-        document.getElementById("construction").style.color = "rgb(0, 0, 0)";
-        
+        document.getElementById("construction").style.color = "rgb(0, 0, 0)";        
     }
 }
